8063e70d30bb433ea1336b4cf2b68295b78f4229
Furqan01510 <furqan01510@gmail.com>
2023-02-22 06:58:43 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/4. Practice - TypeOf.js
@@ -0,0 +1,41 @@
+var a = 1 / 0;
+console.log(a);
+
+// Infinity
+console.log(Infinity + 1);
+console.log(Infinity * 2);
+console.log(1 / Infinity);
+
+// Math.pow 
+Math.pow(10, 2);
+// console.log();
+
+// MAX_VALUE 
+console.log(Number.MAX_VALUE);
+console.log(String.MAX_VALUE);
+console.log(Boolean.MAX_VALUE);
+
+// NaN
+// Not A Number
+parseInt("aaaaaa");
+
+// typeof 
+// is an Operator
+var aVar = 10;
+console.log(typeof aVar);
+
+aVar = true;
+console.log(typeof aVar);
+
+aVar = '999';
+console.log(typeof aVar);
+
+var b;
+console.log(typeof b);
+
+console.log(typeof Infinity);
+
+aVar = null;
+console.log(typeof aVar);
+
+console.log(typeof NaN);
