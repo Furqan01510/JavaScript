33807b665460298515f7841d3cc855e5c2f570b7
Furqan01510 <furqan01510@gmail.com>
2023-02-23 06:59:38 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/9. Practice - Loop.js
@@ -0,0 +1,24 @@
+// Loop
+console.log('Loop');
+
+console.log('For Loop');
+
+var i =1;
+for(;i<=5;i++){
+  console.log(i);
+}
+
+
+console.log('while Loop');
+i=1;
+while(i<=5){
+  console.log(i);
+  i++;
+}
+
+console.log('Do while Loop');
+i=1;
+do{
+  console.log(i);
+  i++;
+}while(i<=5);
