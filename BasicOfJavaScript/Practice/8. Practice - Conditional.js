17dcf45936a1f95b2550f2b76c181419cd9cbaa0
Furqan01510 <furqan01510@gmail.com>
2023-02-23 06:59:30 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/8. Practice - Conditional.js
@@ -0,0 +1,48 @@
+// Conditionals
+console.log('Conditionals');
+
+var aCon = 10;
+
+// if(a>10){
+//   console.log('Positive');
+// }else{
+//   console.log('Negative');
+// }
+
+if (aCon > 0) {
+  console.log('Positive');
+} else if (aCon < 0) {
+  console.log('Negative');
+} else {
+  console.log('Zero');
+}
+
+var bCon = -2;
+if(bCon){
+  console.log('True');
+}else{
+  console.log('False');
+}
+
+var bCon = "abc";
+if(bCon){
+  console.log('True');
+}else{
+  console.log('False');
+}
+
+
+var bCon = "";
+if(bCon){
+  console.log('True');
+}else{
+  console.log('False');
+}
+
+var bCon = null;
+if(bCon){
+  console.log('True');
+}else{
+  console.log('False');
+}
+
