bf6e3863bc3d6a62106e5d215e283c287fb5af5d
Furqan01510 <furqan01510@gmail.com>
2023-02-23 06:59:15 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/6. Practice - Type Coercion.js
@@ -0,0 +1,25 @@
+// Type Coercion
+var aRes;
+
+console.log('Concatination');
+
+aRes = 'Hello' + 'Hello';
+console.log(aRes);
+
+aRes = 'Hello' + 2;
+console.log(aRes);
+
+aRes = '1' + 1;
+console.log(aRes);
+
+// Automatic Type Conversion
+console.log('Automatic Type Conversion');
+aRes = '1' - 1;
+console.log(aRes);
+
+aRes = '2' * 5;
+console.log(aRes);
+
+aRes = '2' / 5;
+console.log(aRes);
+
