f45d01d049a531b7ee563cec7b120ce27d746814
Furqan01510 <furqan01510@gmail.com>
2023-02-24 07:00:29 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/13. Practice - Functions.js
@@ -0,0 +1,32 @@
+// Functions
+var result;
+
+function showAlert1() {
+  alert("hello WOrld");
+}
+showAlert1();
+
+
+function showAlert2() {
+  alert("hello WOrld");
+}
+showAlert2();
+
+function showAlert3(msg) {
+  alert(msg);
+}
+showAlert("Hello!!");
+
+function sum1(a, b) {
+  console.log(a + b);
+}
+result = sum1(10, 5);
+console.log(result);
+
+function sum2(a, b) {
+  return a + b;
+}
+result = sum2(20, 30);
+console.log(result);
+
+
