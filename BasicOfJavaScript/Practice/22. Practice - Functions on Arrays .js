1484c7ccba89090968ed9b38ca686414c1e083e3
Furqan01510 <furqan01510@gmail.com>
2023-02-26 07:02:15 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/22. Practice - Functions on Arrays .js
@@ -0,0 +1,64 @@
+// Functions on Arrays
+
+// Arrays in JavaScript is hetrogenous 
+// Arrays is store in memmory as Object
+
+var arrObj = ['abc', 10, true, 20];
+
+var arr = [45,'Ninja'];
+
+arr = arrObj;
+
+arr.pus(11);
+arr.pop();
+arr.shift();    // Left Shift
+arr.unshift();  // Right Shift
+
+
+// The following methods mutate the original array:
+
+// copyWithin()
+// fill()
+// pop()
+// push()
+// reverse()
+// shift()
+// sort()
+// splice()
+// unshift()
+
+
+//  Arrays Functions
+
+// concat()
+// copyWithin()
+// every()
+// filter()
+// flat()
+// flatMap()
+// forEach()
+// indexOf()
+// lastIndexOf()
+// map()
+// reduce()
+// reduceRight()
+// reverse()
+// slice()
+// some()
+// sort()
+// splice()
+
+// The following methods are iterative:
+
+// every()
+// filter()
+// find()
+// findIndex()
+// findLast()
+// findLastIndex()
+// flatMap()
+// forEach()
+// group()
+// groupToMap()
+// map()
+// some()
