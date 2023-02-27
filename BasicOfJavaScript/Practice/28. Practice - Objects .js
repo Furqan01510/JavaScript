e9e3df285fc64fd23e874730b567b91502d70065
Furqan01510 <furqan01510@gmail.com>
2023-02-27 07:03:33 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/28. Practice - Objects .js
@@ -0,0 +1,18 @@
+// Objects
+
+var student = {
+    name: 'abc',
+    rollNo: 512,
+    marks: 90
+};
+
+// To Access the Keys
+student.name;
+student.rollNo;
+student.marks;
+
+// To insert new keys and values
+student.sem = 4;
+
+// To Display the complete object in console
+student;
