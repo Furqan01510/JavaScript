26bb228fbe0e25a813f7431c1460aea0ce682bd3
Furqan01510 <furqan01510@gmail.com>
2023-03-02 07:06:38 +0530

    JavaScript

---
+++ BasicOfJavaScript/Practice/42. Practice - DOM .js
@@ -0,0 +1,13 @@
+// DOM - Document Object Model
+
+// Accessing DOM
+document.documentElement;   // for html
+document.head;  // Head
+document.body;  // Body
+
+window.document;    // Window is top of document
+window.screen;      // Scren detail of screen
+
+// DOM is Programming API
+// Application Programming Interface
+
